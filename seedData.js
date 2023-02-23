7c0dcab2281f73cc41624afd49ea32ea6893135f
anabelaSantos <anabelamsantos@gmail.com>
2023-02-23 16:18:42 +0000

    part 3 completed

--- seedData.js
+++ seedData.js
@@ -1,20 +1,22 @@
 const seedRestaurant = [
-    {
-      name: 'AppleBees',
-      location: 'Texas',
-      cuisine: 'FastFood'
-    },
-    {
-      name: 'LittleSheep',
-      location: 'Dallas',
-      cuisine: 'Hotpot'
-    },
-    {
-      name: 'Spice Grill',
-      location: 'Houston',
-      cuisine: 'Indian'
-    }
-  ]
-
+  {
+    name: "AppleBees",
+    location: "Texas",
+    cuisine: "FastFood",
+  },
+  {
+    name: "LittleSheep",
+    location: "Dallas",
+    cuisine: "Hotpot",
+  },
+  {
+    name: "Spice Grill",
+    location: "Houston",
+    cuisine: "Indian",
+  },
+];
 
 module.exports = seedRestaurant;
+// exports is just a variable that gets initialized to the value of module.exports before the module is evaluated.
+// That value is a reference to an object (empty object in this case). This means that exports holds a reference to the same object referenced by module.exports.
+// It also means that by assigning another value to exports it's no longer bound to module.exports.
--- server.js
+++ server.js
@@ -1,17 +1,58 @@
-const express = require("express");
-const app = express();
-const { Restaurant } = require("./models/index");
+//  First we invoke the require() function, specifying the name of the module as a string ('express'), and calling the returned object to create an Express application
+// We can then access the properties and functions of the application object.
+const express = require("express"); //import the express module ()
+const app = express(); //create an express application
+// Express itself is a module, as are the middleware and database libraries that we use in our Express applications.
+const { Restaurant } = require("./models/index"); //import the Restaurant model
 const { sequelize } = require("./db");
 
 const port = 3000;
 
 //TODO: Create your GET Request Route Below:
-app.get("/restaurants/:id", async (req, res, next) => {
-  const restaurant = await Restaurant.findByPk(req.params.id);
+// Route definition - The app.get() method specifies a callback function that will be invoked whenever there is an HTTP GET request with a path ('/') relative to the site root.
+// The callback function takes a request and a response object as arguments, and calls send() on the response to return the object restaurant in a json envelope
+app.get("/restaurants/:id", async (req, res) => {
+  const restaurant = await Restaurant.findAll();
   res.json(restaurant);
 });
 
-app.listen(port, () => {
-  sequelize.sync();
-  console.log("Your server is listening on port " + port);
+app.use(express.json());
+app.post("/restaurants", async (req, res, next) => {
+  //next() can be used to pass control to the next handler
+  const { name, location, cuisine } = req.body;
+  if ((name, location, cuisine)) {
+    await Restaurant.create({
+      name: name,
+      location: location,
+      cuisine: cuisine,
+    });
+  }
+  res.json(201);
+});
+
+app.use(express.json());
+app.put("/restaurants/:id", async (req, res, next) => {
+  //next() can be used to pass control to the next handler
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  restaurant.update({
+    name: req.body.name,
+    location: req.body.location,
+    cuisine: req.body.cuisine,
+  });
+  res.json("Put Successful!");
+});
+
+app.use(express.json());
+app.delete("/restaurants/:id", async (req, res, next) => {
+  //next() can be used to pass control to the next handler
+  const restaurant = await Restaurant.findByPk(req.params.id);
+  restaurant.destroy();
+  res.json("Delete Successful!");
+});
+
+// This final block starts up the server on a specified port ('3000') and prints a log comment to the console.
+//create server to listen on port 5000
+const server = app.listen(3000, function () {
+  // the listen() method listens for connections on the host and the port number
+  console.log("Node server is running on http://localhost:3000..");
 });
