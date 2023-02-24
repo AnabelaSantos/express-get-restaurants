69935f4a9b9907a266e90022fabadf8ea32af370
anabelaSantos <anabelamsantos@gmail.com>
2023-02-24 00:23:37 +0000

    part 4 completed

--- server.js
+++ server.js
@@ -3,52 +3,15 @@
 const express = require("express"); //import the express module ()
 const app = express(); //create an express application
 // Express itself is a module, as are the middleware and database libraries that we use in our Express applications.
-const { Restaurant } = require("./models/index"); //import the Restaurant model
+// const { Restaurant } = require("./models/index"); //import the Restaurant model
 const { sequelize } = require("./db");
 
 const port = 3000;
-
-//TODO: Create your GET Request Route Below:
-// Route definition - The app.get() method specifies a callback function that will be invoked whenever there is an HTTP GET request with a path ('/') relative to the site root.
-// The callback function takes a request and a response object as arguments, and calls send() on the response to return the object restaurant in a json envelope
-app.get("/restaurants/:id", async (req, res) => {
-  const restaurant = await Restaurant.findAll();
-  res.json(restaurant);
-});
-
 app.use(express.json());
-app.post("/restaurants", async (req, res, next) => {
-  //next() can be used to pass control to the next handler
-  const { name, location, cuisine } = req.body;
-  if ((name, location, cuisine)) {
-    await Restaurant.create({
-      name: name,
-      location: location,
-      cuisine: cuisine,
-    });
-  }
-  res.json(201);
-});
 
-app.use(express.json());
-app.put("/restaurants/:id", async (req, res, next) => {
-  //next() can be used to pass control to the next handler
-  const restaurant = await Restaurant.findByPk(req.params.id);
-  restaurant.update({
-    name: req.body.name,
-    location: req.body.location,
-    cuisine: req.body.cuisine,
-  });
-  res.json("Put Successful!");
-});
+const restaurantRouter = require("./routers/restRouter");
 
-app.use(express.json());
-app.delete("/restaurants/:id", async (req, res, next) => {
-  //next() can be used to pass control to the next handler
-  const restaurant = await Restaurant.findByPk(req.params.id);
-  restaurant.destroy();
-  res.json("Delete Successful!");
-});
+app.use("/restaurants", restaurantRouter);
 
 // This final block starts up the server on a specified port ('3000') and prints a log comment to the console.
 //create server to listen on port 5000
